25fdab98070abdd7c7770908d5b93e23bbed1465
fauzaanu <fauzaangasim@gmail.com>
2023-12-18 22:30:48 +0500

    due amount fix

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def process_subscription_pabbly():
     # Extract relevant data
     _subscription = api_data["subscription"]
     invoice = api_data["invoice"]
-    amount = invoice["charge_amount"]
+    amount = invoice["due_amount"]
     invoice_id = invoice["id"]
     amount *= 100  # because cents
 
